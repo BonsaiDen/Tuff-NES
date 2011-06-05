9b6b8607083e1ca1c8973ba015664260a1133a59
Ivo Wetzel <ivo.wetzel@googlemail.com>
2011-06-05 04:12:48 +0200

    At the moment we don't need a 2nd program bank.

--- tuff.asm
+++ tuff.asm
@@ -1,7 +1,7 @@
 .segment "HEADER"
         
         .byte   "NES", $1A      ; iNES header identifier
-        .byte   2               ; 2x 16KB PRG code
+        .byte   1               ; 2x 16KB PRG code
         .byte   1               ; 1x  8KB CHR data
         .byte   $01, $00        ; mapper 0, vertical mirroring
 
