7ee40421d184fc50ec65a30b4fcfad3eed5346c9
Ivo Wetzel <ivo.wetzel@googlemail.com>
2011-06-07 00:06:47 +0200

    Get the graphic conversion working again!

--- data.asm
+++ data.asm
@@ -1,16 +1,6 @@
 
 data_palette:
-        ;; Background palette
-        .byte   $06,$02,$03,$04
-        .byte   $10,$3D,$20,$30
-        .byte   $0F,$39,$3A,$3B
-        .byte   $0F,$3D,$3E,$0F
-
-        ; Sprite palette
         .incbin "graphics/tuff.plt"     ; The tuff palette
-        .byte   $42,$42,$42,$42
-        .byte   $00,$1D,$10,$30
-        .byte   $00,$1D,$10,$30
 
 data_sprites:
         ;vert tile attr horiz
